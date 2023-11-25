ca4fe1bf66d11ef43f4e4e8050d143043f524341
pdywilson <pdywilson@gmail.com>
2023-11-25 00:09:31 +0000

    more chores

--- js/chore.js
+++ js/chore.js
@@ -29,15 +29,5 @@ function init() {
 
 document.addEventListener('DOMContentLoaded', () => {
     init();
-
-    // fix mobile screen
-    // const setHeight = () => {
-    //     document.getElementById("container").style.minHeight = window.innerHeight + "px"
-    // };
-    // let deviceWidth = window.matchMedia("(max-width: 1024px)");
-    // if (deviceWidth.matches) {
-    //     window.addEventListener("resize", setHeight);
-    //     setHeight();
-    // }
 });
 
